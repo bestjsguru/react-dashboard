6b1fcf1f39b807677caa47b766178a382705a431
bestjsguru <dlid3991@gmail.com>
2020-08-25 10:30:57 -0700

    first commit

---
+++ src/pages/display-data/display-data.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'devextreme/data/odata/store';
+
+export default () => (
+  <React.Fragment>
+    <h2 className={'content-block'}>Display Data</h2>
+  </React.Fragment>
+);
---
+++ src/pages/profile/profile.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './profile.scss';
+
+export default () => {
+  return (
+    <React.Fragment>
+      <h2 className={'content-block'}>Profile</h2>
+    </React.Fragment>
+  );
+};
